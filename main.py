ef75ab96156c5e3ff99f39bb3a820d9533363fc9
Nuno Carvalho <nunocarvalho@w4msolutions.com>
2023-12-11 09:18:14 +0000

    feat: #2 - Subfolder

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ Example video plugin that is compatible with Kodi 19.x "Matrix" and above
 """
 import sys
 from urllib.parse import urlencode, parse_qsl
-import xbmcgui
-import xbmcplugin
+import xbmc # pylint: disable=E0401
+import xbmcgui # pylint: disable=E0401
+import xbmcplugin # pylint: disable=E0401
 import requests
 
 # Get the plugin url in plugin:// notation.
@@ -16,8 +17,8 @@ _URL = sys.argv[0]
 # Get the plugin handle as an integer number.
 _HANDLE = int(sys.argv[1])
 
-server_url = xbmcplugin.getSetting( _HANDLE, "url" )
-server_authorization = xbmcplugin.getSetting( _HANDLE, "authorization" )
+SERVER_URL = xbmcplugin.getSetting( _HANDLE, "url" )
+SERVER_AUTH = xbmcplugin.getSetting( _HANDLE, "authorization" )
 
 # Free sample videos are provided by www.vidsplay.com
 # Here we use a fixed set of properties simply for demonstrating purposes
@@ -35,10 +36,33 @@ def get_url(**kwargs):
     :return: plugin call URL
     :rtype: str
     """
-    return '{}?{}'.format(_URL, urlencode(kwargs))
+    return '{}?{}'.format(_URL, urlencode(kwargs)) # pylint: disable=C0209
 
+def get_folders():
+    """
+    Get the list of video folders.
 
-def get_categories():
+    Here you can insert some parsing code that retrieves
+    the list of video categories (e.g. 'Movies', 'TV-shows', 'Documentaries' etc.)
+    from some site or API.
+
+    .. note:: Consider using `generator functions <https://wiki.python.org/moin/Generators>`_
+        instead of returning lists.
+
+    :return: The list of video categories
+    :rtype: types.GeneratorType
+    """
+    response = requests.get(
+        f'{SERVER_URL}/api/v1/list-folders/folders',
+        headers={'Authorization': SERVER_AUTH},
+        timeout=60
+        )
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+
+def get_folder_categories(folder):
     """
     Get the list of video categories.
 
@@ -52,14 +76,18 @@ def get_categories():
     :return: The list of video categories
     :rtype: types.GeneratorType
     """
-    response = requests.get(server_url + '/api/v1/list-files/categories', headers={'Authorization': server_authorization})
+    response = requests.get(
+        f'{SERVER_URL}/api/v1/list-folders/folders/{folder}',
+        headers={'Authorization': SERVER_AUTH},
+        timeout=60
+        )
     if response.status_code == 200:
         return response.json()
     else:
         return []
 
 
-def get_videos(category):
+def get_videos(folder, category):
     """
     Get the list of videofiles/streams.
 
@@ -74,30 +102,30 @@ def get_videos(category):
     :return: the list of videos in the category
     :rtype: list
     """
-
-    response = requests.get(server_url + '/api/v1/list-files/categories/' + category, headers={'Authorization': server_authorization})
+    xbmc.log(category, 2)
+    response = requests.get(
+        f'{SERVER_URL}/api/v1/list-files/categories/{folder}/{category}',
+        headers={'Authorization': SERVER_AUTH},
+        timeout=60
+    )
     if response.status_code == 200:
         return response.json()
     else:
         return []
 
 
-def list_categories():
+def list_folder_categories(category):
     """
     Create the list of video categories in the Kodi interface.
     """
-    # Set plugin category. It is displayed in some skins as the name
-    # of the current section.
     xbmcplugin.setPluginCategory(_HANDLE, 'My Video Collection')
-    # Set plugin content. It allows Kodi to select appropriate views
-    # for this type of content.
     xbmcplugin.setContent(_HANDLE, 'videos')
-    # Get video categories
-    categories = get_categories()
-    # Iterate through categories
-    for category in categories:
+
+    categories = get_folder_categories(category)
+
+    for subcategory in categories:
         # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=category)
+        list_item = xbmcgui.ListItem(label=subcategory)
         # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
         # Here we use the same image for all items for simplicity's sake.
         # In a real-life plugin you need to set each image accordingly.
@@ -110,12 +138,12 @@ def list_categories():
         # For available properties see the following link:
         # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
         # 'mediatype' is needed for a skin to display info for this ListItem correctly.
-        list_item.setInfo('video', {'title': category,
-                                    'genre': category,
+        list_item.setInfo('video', {'title': subcategory,
+                                    'genre': subcategory,
                                     'mediatype': 'video'})
         # Create a URL for a plugin recursive call.
         # Example: plugin://plugin.video.example/?action=listing&category=Animals
-        url = get_url(action='listing', category=category)
+        url = get_url(action='listing', folder=category ,category=subcategory)
         # is_folder = True means that this item opens a sub-list of lower level items.
         is_folder = True
         # Add our item to the Kodi virtual folder listing.
@@ -126,7 +154,51 @@ def list_categories():
     xbmcplugin.endOfDirectory(_HANDLE)
 
 
-def list_videos(category):
+def list_folders():
+    """
+    Create the list of video categories in the Kodi interface.
+    """
+    # Set plugin category. It is displayed in some skins as the name
+    # of the current section.
+    xbmcplugin.setPluginCategory(_HANDLE, 'My Video Collection')
+    # Set plugin content. It allows Kodi to select appropriate views
+    # for this type of content.
+    xbmcplugin.setContent(_HANDLE, 'videos')
+    # Get video categories
+    folders = get_folders()
+    # Iterate through categories
+    for folder in folders:
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=folder)
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        list_item.setArt({'thumb': CATEGORY_ART,
+                          'icon': CATEGORY_ART,
+                          'fanart': CATEGORY_ART})
+        # Set additional info for the list item.
+        # Here we use a category name for both properties for for simplicity's sake.
+        # setInfo allows to set various information for an item.
+        # For available properties see the following link:
+        # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
+        # 'mediatype' is needed for a skin to display info for this ListItem correctly.
+        list_item.setInfo('video', {'title': folder,
+                                    'genre': folder,
+                                    'mediatype': 'video'})
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=listing&category=Animals
+        url = get_url(action='category', folder=folder)
+        # is_folder = True means that this item opens a sub-list of lower level items.
+        is_folder = True
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_HANDLE)
+
+
+def list_videos(folder, category):
     """
     Create the list of playable videos in the Kodi interface.
 
@@ -140,7 +212,7 @@ def list_videos(category):
     # for this type of content.
     xbmcplugin.setContent(_HANDLE, 'videos')
     # Get the list of videos in the category.
-    videos = get_videos(category)
+    videos = get_videos(folder, category)
     # Iterate through videos.
     for video in videos:
         # Create a list item with a text label and a thumbnail image.
@@ -153,12 +225,17 @@ def list_videos(category):
         # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
         # Here we use the same image for all items for simplicity's sake.
         # In a real-life plugin you need to set each image accordingly.
-        list_item.setArt({'thumb': video['thumb'], 'icon': video['thumb'], 'fanart': video['thumb']})
+        list_item.setArt({
+            'thumb': video['thumb'],
+            'icon': video['thumb'],
+            'fanart': video['thumb']
+        })
         # Set 'IsPlayable' property to 'true'.
         # This is mandatory for playable items!
         list_item.setProperty('IsPlayable', 'true')
         # Create a URL for a plugin recursive call.
-        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
+        # Example:
+        # plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
         url = get_url(action='play', video=video['video'])
         # Add the list item to a virtual Kodi folder.
         # is_folder = False means that this item won't open any sub-list.
@@ -197,9 +274,12 @@ def router(paramstring):
     params = dict(parse_qsl(paramstring))
     # Check the parameters passed to the plugin
     if params:
-        if params['action'] == 'listing':
+        if params['action'] == 'category':
             # Display the list of videos in a provided category.
-            list_videos(params['category'])
+            list_folder_categories(params['folder'])
+        elif params['action'] == 'listing':
+            # Display the list of videos in a provided category.
+            list_videos(params['folder'], params['category'])
         elif params['action'] == 'play':
             # Play a video from a provided URL.
             play_video(params['video'])
@@ -207,11 +287,9 @@ def router(paramstring):
             # If the provided paramstring does not contain a supported action
             # we raise an exception. This helps to catch coding errors,
             # e.g. typos in action names.
-            raise ValueError('Invalid paramstring: {}!'.format(paramstring))
+            raise ValueError('Invalid paramstring: {paramstring}!')
     else:
-        # If the plugin is called from Kodi UI without any parameters,
-        # display the list of video categories
-        list_categories()
+        list_folders()
 
 
 if __name__ == '__main__':
